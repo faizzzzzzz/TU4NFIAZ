91946b5302b8bd7b2c4f058df5cd7c30b3432e4d
Faizn <50199244+faizzzzzzz@users.noreply.github.com>
2019-06-02 18:05:42 +0700

    Add files via upload

--- TU4NFIAZ.sh
+++ TU4NFIAZ.sh
@@ -24,7 +24,33 @@ echo  $i"6"."hack fb nebak password"
 echo $me"7"."RED HAWK"
 echo $ku"8"."OSIF (curi data fb )"
 echo  $i"9"."weeman (membuat phising sesuka anda)"
-echo $cy"10"."exit/keluar"
+echo $me"10"."Lazymux"
+echo $ku"11"."menampilkan animasi kereta"
+echo  $i"12"."menampilkan spesifikasi android anda"
+echo $me"13"."sqlmap"
+echo $ku"14"."Hash buster"
+echo  $i"15"."Recondog"
+echo $me"16"."install hunner"
+echo $ku"17"."hakku"
+echo  $i"18"."Hash buster"
+echo $me"19"."tembak xl ea"
+echo $ku"20"."admin finder"
+echo  $i"21"."install mrv"
+echo $me"22"."TOOL DTECT"
+echo $ku"23"."cloning yahoo"
+echo  $i"24"."kumpulan web vuln"
+echo $me"25"."hack gmail /root"
+echo $ku"26"."hack wifi tetangga"
+echo  $i"27"."keadaan cuaca hari ini"
+echo $me"28"."musikan wkwkwkw"
+echo $ku"29"."hack instagram"
+echo  $i"30"."install webdav"
+echo $me"31"."install metasploit"
+echo $ku"32"."install social fish"
+echo  $i"33"."install scaner inurl"
+echo $me"34"."install crew bot"
+echo $ku"35"."install kktp gratis"
+echo $cy"36"."exit/keluar"
 echo
 echo $pur"Ôò¡ÔöÇ["$pur"Masukkan pilihan anda"$pur"]"
 read -p"Ôò¦ÔöÇ>> " pil
@@ -32,7 +58,7 @@ read -p"Ôò¦ÔöÇ>> " pil
 if [ $pil = 1 ]
 then
 clear
-sleep1
+sleep 1
 pkg install python2
 pkg install git
 git clone https://github.com/faizzzzzzz/ddos
@@ -43,7 +69,7 @@ fi
 if [ $pil = 2 ]
 then
 clear
-sleep1
+sleep 1
 pkg install php
 pkg install git
 git clone https://github.com/faizzzzzzz/smscaping
@@ -54,7 +80,7 @@ fi
 if [ $pil = 3 ]
 then
 clear
-sleep1
+sleep 1
 pkg insall php
 pkg install git
 git clone https://github.com/zlucifer/troll_project
@@ -65,7 +91,7 @@ fi
 if [ $pil = 4 ]
 then
 clear
-sleep1
+sleep 1
 pkg install python2
 pkg install git
 git clone https://github.com/4L13199/LITESCRIPT
@@ -76,7 +102,7 @@ fi
 if [ $pil = 5 ]
 then
 clear
-sleep1
+sleep 1
 pkg install python2
 pkg install git
 git clone https://github.com/pirmansx/mbf
@@ -87,7 +113,7 @@ fi
 if [ $pil = 6 ]
 then
 clear
-sleep1
+sleep 1
 pkg install python2
 pkg install git
 pkg install git https://github.com/pirmansx/mbf
@@ -98,7 +124,7 @@ fi
 if [ $pil = 7 ]
 then
 clear
-sleep1
+sleep 1
 pkg install php
 pkg install git
 git clone https://github.com/Tuhinshubhra/RED_HAWK
@@ -109,7 +135,7 @@ fi
 if [ $pil = 8 ]
 then
 clear
-sleep1
+sleep 1
 pkg install python2
 pkg install git
 git clone https://github.com/CiKu370/OSIF
@@ -120,7 +146,7 @@ fi
 if [ $pil = 9 ]
 then
 clear
-sleep1
+sleep 1
 pkg install python2
 pkg install git
 git clone https://github.com/evait-security/weeman
@@ -128,14 +154,368 @@ cd weeman
 python2 weeman.py
 fi
 
-if [ $pil = 10 ]
+
+if  [ $pil = 10 ]
+then
+clear
+sleep 1
+pkg install python2
+pkg install git
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+python2 lazymux.py
+fi
+
+
+if  [ $pil = 11 ]
+then
+clear
+sleep 1
+pkg install sl
+sl
+fi
+
+if  [ $pil = 12 ]
+then
+clear
+sleep 1
+pkg install neofetch
+neofetch
+fi
+
+if  [ $pil = 13 ]
+then
+clear
+sleep 1
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap
+https://github.com/sqlmapproject/sqlmap.git
+cd sqlmap
+Python2 sqlmap.py
+fi
+
+if  [ $pil = 14 ]
+then
+clear
+sleep 1
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/Hash-Buster
+cd Hash-Buster
+python2 hash.py
+fi
+
+if  [ $pil = 15 ]
+then
+clear
+sleep 1
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/ReconDog
+cd ReconDog
+chmod +x dog.py
+Python2 dog.py
+fi
+
+if  [ $pil = 16 ]
+then
+clear
+sleep 1
+pkg install git
+git clone https://github.com/b3-v3r/Hunner
+ cd Hunner
+ chmod 777 hunner.py
+ python hunner.py
+fi
+
+if  [ $pil = 17 ]
+then
+clear
+sleep 1
+apt install git
+mkdir vasu
+git clone https://github.com/4shadoww/hakkuframework
+cd hakkuframework
+chmod +x hakku
+python hakku
+show modules
+use whois
+show options
+fi
+
+if  [ $pil = 18 ]
+then
+clear
+sleep 1
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/Hash-Buster
+cd Hash-Buster
+python2 hash.py
+fi
+
+
+
+if  [ $pil = 19 ]
+then
+clear
+sleep 1
+pkg upgrade
+pkg update
+pkg install git
+pkg install python
+git clone https://github.com/albertoanggi/xl-py
+pip install -r requirements.txt
+chmod +x app.py
+python/python2 app.py
+fi
+
+if  [ $pil = 20 ]
+then
+clear
+sleep 1
+pkg install python2
+pkg install git
+git clone https://github.com/AdheBolo/AdminFinder
+ls
+cd AdminFinder
+chmod 777 AdminFinder.py
+python2 AdminFinder.py
+fi
+
+if  [ $pil = 21 ]
+then
+clear
+sleep 1
+pkg install git
+pkg install gem
+pkg install figlet
+gem install lolcat
+git clone https://github.com/Mr-R225/Mr.Rv1.1
+cd Mr.Rv1.1
+sh Mr.Rv1.1.sh
+fi
+
+if  [ $pil = 22 ]
+then
+clear
+sleep 1
+apt install git
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECT
+apt install python2
+chmod +x d-tect.py
+python2 d-tect.py
+fi
+
+if  [ $pil = 23 ]
+then
+clear
+sleep 1
+pkg install git python python2 php curl clang
+git clone https://github.com/wahyuandhika/YahooCloning
+cd YahooCloning
+ls
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+
+if  [ $pil = 24 ]
+then
+clear
+sleep 1
+hhttp://www.6y555.com
+http://www.nuclear-experts.net
+http://www.anecra.pt
+http://209.60.104.53
+http://baza.ds.org.rs
+http://oulitnet.co.za
+http://liteoptec.co.za
+http://0739.gwytk.com
+http://hr.systematixshares.com
+http://leader.gwytk.com
+http://www.rabachino.com
+http://b2b.modelberg.it
+http://hengyue-media.com
+http://webphone.zayhi.com
+http://koiis.co.kr
+http://223.255.137.119
+http://sys.betafac.com
+http://gateway.net.co
+http://hq.prospec.co.th
+http://branch.prospec.co.th
+http://rso.gie-vso.fr
+http://112acil.com
+http://maestrodascia.eu
+http://mediacom.biz
+http://tciglobal.com
+http://0731.zgxxk.net
+http://0745.gwytk.com
+http://bestilling.utdanningsdirektoratet.no
+http://www.ldgb.net
+http://www.zgxxk.net
+http://www.567888.pw
+http://www.danahias.at
+http://www.wsdscu.org
+http://bestilling.udir.no
+http://scm.icams.co.kr
+http://158.36.153.158
+http://tirupurjobs.net
+http://www.apparelworld.org
+http://www.tirupurjobs.net
+http://apparelworld.org
+http://gma2t.wlannet.net
+http://praso.wlannet.net
+http://smsget.zayhi.com
+http://tctnh.wlannet.net
+http://webphone.zayhi.com
+http://wop.zayhi.com
+fi
+
+if [ $pil = 25 ]
+then
+clear
+sleep 1
+apt-get install git
+apt-get install python python-pip python-setuptools
+pip install scapy
+git clone https://github.com/wifiphisher/wifiphisher.git
+cd wifiphisher
+python setup.py install
+cd wifiphisher
+python wifiphisher
+fi
+
+if [ $pil = 26 ]
+then
+clear
+sleep 1
+apt install git
+git clone https://github.com/esc0rtd3w/wifi-hacker
+ls
+cd wifi-hacker
+ls
+chmod +x wifi-hacker.sh
+ls
+wifi-hacker.sh
+fi
+
+if [ $pil = 27 ]
+then
+clear
+sleep 1
+curl http://wttr
+fi
+
+if [ $pil = 28 ]
+then
+clear
+sleep 1
+pip install mps_youtube
+pip install youtube_dl
+apt install mpv
+mpsyt
+fi
+
+if [ $pil = 29 ]
+then
+clear
+sleep 1
+pkg install git
+pkg install nano
+git clone https://github.com/avramit/instahack.git
+ls
+cd instahack
+ls
+pip install requests
+cd instahack
+nano pass.txt
+fi
+
+if [ $pil = 30 ]
+then
+clear
+sleep 1
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+apt install openssl curl
+pkg install libcurl
+ln -s /sdcard
+cd sdcard
+mkdir webdav
+cd webdav
+fi
+
+if [ $pil = 31 ]
+then
+clear
+sleep 1
+pkg install curl
+pkg install python
+pkg install python2
+git clone https://raw.githubusercontent.com/Techzindia/Metasploit_For_Termux/master/metasploitTechzindia.sh
+ls
+chmod 777 metasploitTechzindia.sh
+metasploitTechzindia.sh
+fi
+
+if [ $pil = 32 ]
+then
+clear
+sleep 1
+pkg install git
+pkg install python2
+pkg install php
+git clone https://github.com/UndeadSec/SocialFish
+cd SocialFish
+pip install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $pil = 33 ]
+then
+clear
+sleep 1
+pkg install php
+pkg install git
+git clone https://github.com/googleinurl/SCANNER-INURLBR.git
+cd SCANNER-INURLBR
+php inurlbr.php
+fi
+
+if [ $pil = 34 ]
+then
+clear
+sleep 1
+pkg install git
+git clone https://github.com/Xeit666h05t/CrewBot
+ls
+cd CrewBot
+python2 CrewBot.py
+fi
+
+if [ $pil = 35 ]
+then
+clear
+sleep 1
+pkg install git
+pkg install php
+git clone https://github.com/zerosvn/ktpkkgenerate
+ls
+cd ktpkkgenerate
+ls
+php zerosvn.php
+fi
+
+if [ $pil = 36 ]
 then
 clear
 figlet -f slant "E X I T"|lolcat
 sleep 2
 echo $cy"Terima Kasih Sudah Pakai Tool Saya"
 sleep 2
-echo $i"share ke temen kalian jangan lupa yapsss!!"
+echo $i"share ke temen kalian jangan lupa yaps!!"
 sleep 2
 echo $ku"contact :"$i" 085220205961"
 echo $ku"nama :"$i" Mr.f"
